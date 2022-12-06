88013a138dacc95f3a882ef450dfcac280cc4b36
Rogelio Gomez <ing.rogelio.gomez@gmail.com>
2022-12-05 22:41:00 -0600

    Practica Modulo 12
    Termine los ejercicios.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool isGameStarted = false;
+    [SerializeField] private float ballSpeed = 10.5f;
+
     void Start()
     {
-        
+        Vector3 initialPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+        initialPosition.y += 3;
+        this.transform.position = initialPosition;
+        this.transform.SetParent(GameObject.FindGameObjectWithTag("Player").transform);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        MoveBallSpace();
+    }
+
+    // La bola sale disparada del paddle al apretar la tecla space o al apretar a en el control
+    private void MoveBallSpace()
+    {
+        if (Input.GetKey(KeyCode.Space) || Input.GetButton("Submit"))
+        {
+            if (!isGameStarted)
+            {
+                isGameStarted = true;
+                this.transform.SetParent(null);
+                GetComponent<Rigidbody>().velocity = ballSpeed * Vector3.up;
+            }
+        }
     }
 }
